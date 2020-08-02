057618c7098e1d60bfd86d7b97e5ef9097bf31b6
JosueCuba <elian.cuba@vallegrande.edu.pe>
2020-08-01 23:30:09 -0500

    :fire:

---
+++ microservice/src/main/java/pe/edu/vallegrande/microservice/controllers/MainController.java
@@ -0,0 +1,23 @@
+package pe.edu.vallegrande.microservice.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import pe.edu.vallegrande.microservice.models.Greeting;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class MainController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+
+
+}
